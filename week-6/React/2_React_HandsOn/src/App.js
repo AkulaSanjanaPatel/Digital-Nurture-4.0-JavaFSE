e1289c37c5b79d3aa16d03a55bcc0d72b0922bd8
AkulaSanjanaPatel <137385018+AkulaSanjanaPatel@users.noreply.github.com>
2025-07-27 18:27:48 +0530

    Add files via upload

---
+++ week-6/React/2_React_HandsOn/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Home from './Components/Home';
+import About from './Components/About';
+import Contact from './Components/Contact';
+
+function App() {
+  return (
+    <center>
+    <div className="App">
+      <Home />
+      <About />
+      <Contact />
+    </div>
+    </center>
+  );
+}
+
+export default App;
