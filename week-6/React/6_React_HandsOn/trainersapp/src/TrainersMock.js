8fa97cf346859813a91ddc2dd298e08cf5e229f9
AkulaSanjanaPatel <137385018+AkulaSanjanaPatel@users.noreply.github.com>
2025-07-27 19:18:12 +0530

    Add files via upload

---
+++ week-6/React/6_React_HandsOn/trainersapp/src/TrainersMock.js
@@ -0,0 +1,9 @@
+import Trainer from './Trainer';
+
+const trainers = [
+  new Trainer(1, 'Ankit Sharma', 'ankit@cognizant.com', '9876543210', 'React', 'React, JS'),
+  new Trainer(2, 'Nisha Mehta', 'nisha@cognizant.com', '8765432109', 'Spring Boot', 'Java, Spring Boot'),
+  new Trainer(3, 'Raj Verma', 'raj@cognizant.com', '7654321098', 'Angular', 'Angular, Typescript')
+];
+
+export default trainers;
